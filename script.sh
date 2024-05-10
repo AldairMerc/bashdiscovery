3df4ef039ff403014acb80a4d8115d92b4016419
AldairMerc <90732036+AldairMerc@users.noreply.github.com>
2024-05-09 19:56:34 -0600

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Dame el endpoint a atacar: "
+read url
+echo "Como se llama el archivo: "
+read file
+
+echo "Attacking endpoint: $url \n\nHeaders and codes of endpoint: $url"> $file;
+curl -I $url -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' >> file;
+echo "URLS in this endpoint" >> file; 
+curl -I '$url' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' | grep -o 'href"[^"#"]"' | $url = $url awk -F '"' 'if ($2 !~ /http/) print $url; $2; else print $2}' | sort | uniq -u >> $file;
+
