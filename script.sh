5d2bb5a330adee80cb1f10817686c3a02a92c7af
AldairMerc <90732036+AldairMerc@users.noreply.github.com>
2024-05-22 20:38:20 -0600

    Update script.sh
    Se corrige parte del awk quitando la variable de url

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ read file
 echo -e "Attacking endpoint: $url\n\nHeaders and codes of endpoint: $url"> $file;
 curl -I $url -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' >> $file;
 echo "URLS in this endpoint" >> $file; 
-curl $url -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' | grep -o 'href="[^"#"]*"' |$url = $url awk -F '"' '{if($2 !~ /^http/) print $url; else print $url $2}'| sort | uniq -u >> $file
+curl $url -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' | grep -o 'href="[^"#"]*"' |awk -F '"' '{if($2 !~ /^http/) print $url; else print $url $2}'| sort | uniq -u >> $file
 #curl  '$url' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' | grep -o 'href"[^"#"]"' | awk -F '"' '{if ($2 !~ /http/) print $2; else print $2}' | sort | uniq -u >> $file;
